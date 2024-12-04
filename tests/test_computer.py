8e1f4629b6d7c70bee6f93e5d144fbd69e9fa69d
Takumi sakata <no4nixs20@gmail.com>
2024-12-04 10:29:00 +0900

    Add or modify tests for computer.py

--- tests/test_computer.py
+++ tests/test_computer.py
@@ -1,16 +1,46 @@
-import pytest
-from src.computer import add, subtract, multiply, divide
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Takumi Sakata
+# SPDX-License-Identifier: BSD-3-Clause
 
-def test_add():
-    assert add(1, 2) == 3
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Sora Hirano
+# SPDX-License-Identifier: BSD-3-Clause
 
-def test_subtract():
-    assert subtract(2, 1) == 1
+ng () {
+    echo "${1}行目が違うよ"
+    res=1
+}
 
-def test_multiply():
-    assert multiply(2, 3) == 6
+res=0
 
-def test_divide():
-    assert divide(6, 3) == 2
+# テストケース1: 足し算
+out=$(echo 3 2 | python3 ./computer.py add)
+[ "${out}" = "5.0" ] || ng "$LINENO"
 
+# テストケース2: 引き算
+out=$(echo 5 2 | python3 ./computer.py subtract)
+[ "${out}" = "3.0" ] || ng "$LINENO"
+
+# テストケース3: 掛け算
+out=$(echo 2 3 | python3 ./computer.py multiply)
+[ "${out}" = "6.0" ] || ng "$LINENO"
+
+# テストケース4: 割り算
+out=$(echo 6 3 | python3 ./computer.py divide)
+[ "${out}" = "2.0" ] || ng "$LINENO"
+
+# テストケース5: ０で割る
+out=$(echo 5 0 | python3 ./computer.py divide)
+[ "$?" = 1 ] || ng "$LINENO"
+
+# テストケース6: 数字以外の入力
+out=$(echo 5 a | python3 ./computer.py add)
+[ "$?" = 1 ] || ng "$LINENO"
+
+# テストケース7: 空の入力
+out=$(echo | python3 ./computer.py add)
+[ "$?" = 1 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
 
